6020cfe6b93f21c02a5ebd3370b36d485290b4f7
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2019-02-09 14:35:04 -0500

    game.js add recursive function to avoid repeated numbers

---
+++ README.md
@@ -0,0 +1,11 @@
+# __Crystal Collector__
+
+## What Is the Goal of This Project?
+
+## Who Can Use this Repo?
+#### Anyone! :tada:
+
+## Credits
+
+## Notes
+#### This is part of an ongoing school project. Comments and feedbacks are much appreciated! :+1:
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 // NOTE TO SELF:
-// 1. NUMBER OPTIONS NO REPEAT?
 // 2. CHANGE ALL VAR TO STATE.OBJECT TO RESET GAME (REFER TO ASSIGNMENT 3)
 
 // Set target number as a randomly generated value between 19-180:
@@ -14,12 +13,30 @@ var counter = 0;
 var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 var newNumberOptions = [];
 
-for (var i = 0; i < 4; i++) {
-  var pickedNumbers = 1 + Math.floor(Math.random() * numberOptions.length);
-  newNumberOptions.push(pickedNumbers);
-  console.log (pickedNumbers);
+function setNumbersForButtons(){
+  for (var i = 0; i < 4; i++) {
+    var pickedNumbers = 1 + Math.floor(Math.random() * numberOptions.length);
+    if(newNumberOptions.includes(pickedNumbers)){
+      recursiveButtons();
+    }
+    else {  
+      newNumberOptions.push(pickedNumbers);
+    }
+  }
 }
+setNumbersForButtons();
 console.log (newNumberOptions);
+
+// (TA - Pete) using a recursive function outside of For Loop to avoid repeated numbers
+function recursiveButtons(){
+  var newNum = 1 + Math.floor(Math.random() * numberOptions.length);
+  if(newNumberOptions.includes(newNum)){
+    recursiveButtons();
+  }
+  else {
+    newNumberOptions.push(newNum);
+  }
+}
 
 // link values to HTML element (to be put in logical operators):
 $("#number-to-guess").text(targetNumber);
@@ -30,38 +47,38 @@ $("#total-score").text(counter);
 // For Loop to append crystals with class and image attribute
 for (var i = 0; i < newNumberOptions.length; i++) {
 
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "./assets/images/g" + i + ".png");
+  var imageCrystal = $("<img>");
+  imageCrystal.addClass("crystal-image");
+  imageCrystal.attr("src", "./assets/images/g" + i + ".png");
 
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", newNumberOptions[i]);
-    $("#crystals").append(imageCrystal);
-    }
+  // This data attribute will be set equal to the array value.
+  imageCrystal.attr("data-crystalvalue", newNumberOptions[i]);
+  $("#crystals").append(imageCrystal);
+  }
 
 // On-click event for every crystal
 $(".crystal-image").on("click", function() {
 
-    // Determine the crystal's value by extracting the value from data attribute
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
+  // Determine the crystal's value by extracting the value from data attribute
+  // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+  var crystalValue = ($(this).attr("data-crystalvalue"));
+  crystalValue = parseInt(crystalValue);
 
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-    $("#total-score").text(counter);
+  // Every click, from every crystal adds to the global counter.
+  counter += crystalValue;
+  $("#total-score").text(counter);
 
-    // Set win-lose logic
-    if (counter === targetNumber) {
-      alert("You win!");
-      winNumber += 1;
-      $("#win-count").text(winNumber);
-    }
+  // Set win-lose logic
+  if (counter === targetNumber) {
+    alert("You win!");
+    winNumber += 1;
+    $("#win-count").text(winNumber);
+  }
 
-    else if (counter >= targetNumber) {
-      alert("You lose!!");
-      loseNumber += 1;
-      $("#lose-count").text(loseNumber);
-    }
+  else if (counter >= targetNumber) {
+    alert("You lose!!");``
+    loseNumber += 1;
+    $("#lose-count").text(loseNumber);
+  }
 
-  });
+});
